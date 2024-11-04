8708613d4d0fc7608705ac20061bc25229346679
itsluismario <itsluismario@gmail.com>
2024-11-03 22:15:27 -0600

    ♻️ Add initial API endpoint for posts
    - Add mock post data for testing
    - Implement JSON response structure
    - Set up basic route handling in Express app
    
    Response format:
    - message: success confirmation
    - posts: mock post object with id, title, content

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+// app.js
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Poop');
-    
-    next();
-});
-
-app.use((req, res, next) => {
-    res.send('Hola');
+app.use('/api/posts', (req, res, next) => {
+    const posts = {
+        id: 'sdfnad',
+        title: 'First server-side post',
+        content: 'This is coming from the server'
+    };
+    res.json({
+        message: 'Posts fetched succesfully',
+        posts: posts
+    });
 });
 
 module.exports = app;
