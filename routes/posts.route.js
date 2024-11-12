4d4f9e59b39271766fad0a54a360c6338e340d06
itsluismario <itsluismario@gmail.com>
2024-11-12 17:03:00 -0600

    🔨 Implement server-side pagination for posts
    - Add query parameters handling:
      * pagesize: number of posts per page
      * page: current page number
    
    - Implement MongoDB pagination:
      * Add skip() for offset calculation
      * Add limit() for posts per page
      * Maintain existing post mapping
      * Keep response structure consistent
    
    Route: GET /api/posts
    Query params:
    - pagesize: Controls posts per request
    - page: Determines offset
    
    File modified: routes/posts.route.js

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -97,7 +97,15 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.get('', (req, res, next) => {
-    Post.find().then(documents => {
+    const pageSize = +req.query.pagesize;
+    const currentPage = +req.query.page;
+    const postQuery = Post.find();
+    if (pageSize && currentPage) {
+        postQuery
+            .skip(pageSize * (currentPage - 1))
+            .limit(pageSize);
+    }
+    postQuery.then(documents => {
         res.status(200).json({
             message: 'Posts fetched succesfully',
             posts: documents.map(doc => {
