e2b6be8992a2652c314173c312f9ba4db1fe9583
itsluismario <itsluismario@gmail.com>
2024-11-14 17:17:59 -0600

    🔨 Fix user registration functionality by using the correct User model
    Changes included in this commit:
    
    1. Updated the `user.route.js` file to use the `User` model imported from the `../models/user` module.
       - Previously, the code was creating a new `UserActivation` model instance, which was not defined.
       - Now, the correct `User` model is used to create a new user document in the database.
    
    2. Handled the error case where the email is already in use:
       - If a user tries to sign up with an email that already exists, the route will now return a 500 status code with a custom error message ("Email already in use").
       - This provides more meaningful feedback to the client when a registration attempt fails due to a duplicate email.
    
    This commit resolves the issue where the user registration route was not using the correct User model, which was causing errors. It also adds better error handling to inform the client when a registration fails due to a duplicated email address.
    
    1. Created a new `user.route.js` file to define the routes for user registration.
    2. Implemented the `/signup` POST route to handle user registration:
       - Uses the `bcrypt` library to hash the user's password before saving it to the database.
       - Creates a new `UserActivation` model instance with the user's

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const postsRoutes = require('./routes/posts.route');
-const signupRoutes = require('./routes/signup.route');
+const userRoutes = require('./routes/users.route');
 
 require('dotenv').config({ path: '.env.dev' });
 
@@ -42,7 +42,7 @@ app.use((req, res, next) => {
 });
 
 app.use('/api/posts',postsRoutes);
-app.use('/api/signup',signupRoutes);
+app.use('/api/users',userRoutes);
 
 // Error handling middleware for file serving
 app.use((err, req, res, next) => {
