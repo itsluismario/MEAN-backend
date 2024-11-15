dcea2700f789777164e7a3954dad6643b1732ca6
itsluismario <itsluismario@gmail.com>
2024-11-14 18:42:00 -0600

    🔨 Implement user authentication functionality
    1. Added a new `/login` POST route:
       - Finds the user in the database based on the provided email.
       - If the user is not found, returns a 401 (Unauthorized) status code.
       - If the user is found, compares the provided password with the stored hashed password using `bcrypt.compare()`.
       - If the password is valid, generates a JSON Web Token (JWT) using the `jsonwebtoken` library, including the user's email and ID.
       - Returns the generated JWT with a 200 (OK) status code.
       - If the password is invalid, returns a 401 (Unauthorized) status code.

--- routes/users.route.js
+++ routes/users.route.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const jwt = require('jsonwebtoken');
+
 const router = express.Router();
 
 router.post("/signup", (req, res, next) => {
@@ -26,5 +28,40 @@ router.post("/signup", (req, res, next) => {
                 });
         })
 });
+
+router.post('/login', (req, res, next) => {
+    let fetchedUser;
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            if(!user) {
+                return res.status(401).json({
+                    message: 'Auth failed'
+                });
+            }
+            fetchedUser = user;
+            return bcrypt.compare(req.body.password, user.password);
+        })
+    .then(result => {
+        if (!result) {
+            return res.status(401).json({
+                message: 'Auth failed'
+            })
+        }
+        const token = jwt.sign(
+            { email: fetchedUser.email, userId: fetchedUser._id }, 
+            'key_secret_JTW', 
+            { expiresIn: '1h' }
+        )
+        return res.status(200).json({
+            token: token
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(401).json({
+            message: 'Auth failed'
+        });
+    })
+})
 
 module.exports = router;
