c0aa08c8f4f9f7fb29a8abba4410f34d20caf282
itsluismario <itsluismario@gmail.com>
2024-11-16 18:27:30 -0600

    🛂 Implement JWT-based authentication middleware
    Changes included in this commit:
    
    1. Created a new `check-auth.middleware.js` file in the backend:
       - This middleware function verifies the JWT token sent in the `Authorization` header of incoming requests.
       - It extracts the token from the header, verifies it using the `jwt_secret` environment variable, and allows the request to proceed to the next middleware or route handler if the token is valid.
       - If the token is invalid or missing, the middleware returns a 401 Unauthorized response.
    
    2. Integrated the `check-auth` middleware into the existing routes:
       - This ensures that only authenticated users can access sensitive functionality, such as creating or updating posts.
    
    3. Updated the `user.route.js` file:
       - Replaced the hardcoded `'key_secret_JTW'` string used for JWT token generation with an environment variable `JWT_SECRET`.
       - The `JWT_SECRET` environment variable is loaded using the `dotenv` package, allowing the secret key to be stored securely outside of the codebase.
    
    This commit introduces a JWT-based authentication system on the backend. By verifying the JWT token in the `check-auth.middleware.js`, the application can ensure that only authenticated users have access to sensitive functionality.
    
    The use of an environment variable for the JWT secret key also improves the overall security of the application by keeping the secret key separate from the codebase.

--- middleware/check-auth.middleware.js
+++ middleware/check-auth.middleware.js
@@ -7,10 +7,16 @@ const jwt_secret = process.env.JWT_SECRET;
 
 module.exports = (req, res, next) => {
     try {
+        console.log('All headers', req.headers);
+        console.log('Auth0', req.headers.authorization);
         const token = req.headers.authorization.split(' ')[1];
         jwt.verify(token, jwt_secret);
+
+        
         next();
     } catch (error) {
+        console.log(error);
+        
         res.status(401).json({ message: 'Auth failed' });
     }
 };
