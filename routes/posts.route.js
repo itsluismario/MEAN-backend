1ef457719f31513605a1465ef44a431a815df469
itsluismario <itsluismario@gmail.com>
2024-11-19 13:46:28 -0600

    🛂 Add user authorization checks for post operations
    - Add creator verification for updates/deletes
    - Implement proper response handling
    - Return appropriate status codes
    - Add unauthorized access handling
    - Validate operation success

--- routes/posts.route.js
+++ routes/posts.route.js
@@ -81,11 +81,15 @@ router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), (req
     };
     
     Post.updateOne(
-        { _id: req.params.id }, 
+        { _id: req.params.id, creator: req.userData.userId }, 
         { $set: updateData })           
         .then(result => {
-        res.status(200).json({ message: 'Update successful!'});
-    });
+        if (result.modifiedCount > 0) {
+            res.status(200).json({ message: 'Update successful!'});
+        } else {
+            res.status(401).json({ message: 'Not authorized!'});
+        }
+    }); 
 });
 
 router.get('/:id', (req, res, next) => {
@@ -133,10 +137,13 @@ router.get('', (req, res, next) => {
 });
 
 router.delete('/:id', checkAuth,(req, res, next) => {
-    Post.deleteOne({ _id: req.params.id }).then(result => {
-        res.status(200).json({message: 'Post deleted succesfully'}); 
+    Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(result => {        
+        if (result.deletedCount > 0) {
+            res.status(200).json({ message: 'Post deleted succesfully!'});
+        } else {
+            res.status(401).json({ message: 'Not authorized!'});
+        }
     });
-
 });
 
 module.exports = router;
